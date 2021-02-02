7cbdfdd993be05c7ec234dd1d33ef081fdff9ba9
Jonah Meijers <theonejonahgold@users.noreply.github.com>
2021-02-02 16:26:27 +0100

    Update login.js

--- functions/funcs/login.js
+++ functions/funcs/login.js
@@ -3,7 +3,7 @@ const constants = require('../constants.js')
 
 exports.handler = function (event, context) {
   return {
-    statusCode: 301,
+    statusCode: 302,
     headers: {
       Location: `https://accounts.spotify.com/authorize?response_type=code&client_id=${SPOTIFY_CLIENT_ID}&scope=${
         constants.scopes
