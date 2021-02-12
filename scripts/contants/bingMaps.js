12c6fde6e69710afc1db7b91b9336879f439f47e
Jonah Meijers <jonahmeijers97@gmail.com>
2021-02-12 15:14:36 +0000

    chore: update Bing maps constants

--- scripts/contants/bingMaps.js
+++ scripts/contants/bingMaps.js
@@ -1,4 +1,3 @@
 const API_KEY =
   'AqFF99g0-zvgNxa_BwnASnIVfniNjNBuucRUQPbVzOptYQ2eJbK2wKTFDdKXue9b'
-export const LOCATION_URI = `http://dev.virtualearth.net/REST/v1/Locations?key=${API_KEY}&maxResults=1&query=`
-export const ROUTE_URI = `http://dev.virtualearth.net/REST/v1/Routes?&maxSolutions=1&key=${API_KEY}`
+export const ROUTE_URI = `https://dev.virtualearth.net/REST/v1/Routes?&maxSolutions=1&key=${API_KEY}`
